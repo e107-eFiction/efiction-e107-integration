dc1d8fc81d175828cae47a60e1ecc0a4049ba744
Jimmi08 <janamicek@gmail.com>
2021-05-13 09:00:18 +0200

    searchform and series browsing template

--- e107_plugins/efiction/templates/browse_template.php
+++ e107_plugins/efiction/templates/browse_template.php
@@ -14,35 +14,8 @@ $BROWSE_TEMPLATE['index']['body'] =
 </div>
 ';  
 $BROWSE_TEMPLATE['index']['end'] = '';
-$BROWSE_TEMPLATE['index']['tablerender'] = 'blocks_center'; 
-
-
-$BROWSE_TEMPLATE['searchform']['index']  = '
-<section class="property-search-section">
-<div class="auto-container">
-	<div class="property-search-tabs tabs-box">
-		<div class="tabs-content">
-			<!--Tab / Active Tab-->
-			<div class="tab active-tab" id="sale">                         
-				<div class="property-search-form">
-					<div id="sortform">
-                                {BROWSE_SORTBEGIN} 
-						<div class="row">
-							{BROWSE_CATEGORYMENU}
-							{BROWSE_CHARACTERMENU1} {BROWSE_CHARACTERMENU2} {BROWSE_PAIRINGSMENU} {BROWSE_RATINGMENU} 
-                                    {BROWSE_CLASSMENU} 
-                                    {BROWSE_SORTMENU} 
-                                    {BROWSE_COMPLETEMENU} 
-						</div>
-                                {BROWSE_SORTEND}
-					</div>									
-				</div> 
-			</div>
-		</div>							
-	</div>
-</div>
-</section>'; 
-
+$BROWSE_TEMPLATE['index']['tablerender'] = 'browse'; 
+ 
 $BROWSE_TEMPLATE['categories'] = $BROWSE_TEMPLATE['index'];  
 $BROWSE_TEMPLATE['ratings'] = $BROWSE_TEMPLATE['index']; 
 $BROWSE_TEMPLATE['original_authors'] = $BROWSE_TEMPLATE['index'];
@@ -50,11 +23,9 @@ $BROWSE_TEMPLATE['original_titles'] = $BROWSE_TEMPLATE['index'];
 $BROWSE_TEMPLATE['titles'] = $BROWSE_TEMPLATE['index'];
 $BROWSE_TEMPLATE['recent'] = $BROWSE_TEMPLATE['index'];
 $BROWSE_TEMPLATE['home'] = $BROWSE_TEMPLATE['index'];
-
  
-$BROWSE_WRAPPER['series']['BROWSE_SEARCHFORM'] = '
-<div class="col-md-4">{---}</div>';
- 
+// search in sidebar 
+// $BROWSE_WRAPPER['series']['BROWSE_SEARCHFORM'] = '<div class="col-md-4">{---}</div>';
  
 $BROWSE_TEMPLATE['series']['caption'] = ''; 
 $BROWSE_TEMPLATE['series']['start'] = '';
@@ -62,54 +33,15 @@ $BROWSE_TEMPLATE['series']['body'] =
 '
 <div class="row my-3">      
     <div class="col-md-12">
+        {BROWSE_SEARCHFORM} 
 		{BROWSE_ALPHALINKS}
-		{BROWSE_OUTPUT}{BROWSE_OTHERRESULTS}
+		{BROWSE_OUTPUT}
+        {BROWSE_OTHERRESULTS}
 		{BROWSE_SERIESBLOCK}
         {BROWSE_PAGELINKS}
     </div>
 </div>
 ';  
-$BROWSE_TEMPLATE['series']['end'] = '';
-$BROWSE_TEMPLATE['series']['searchform'] = $BROWSE_TEMPLATE['index']['searchform'] ;  
+$BROWSE_TEMPLATE['series']['end'] = ''; 
   
-$BROWSE_TEMPLATE['characters']['caption'] = ''; 
-$BROWSE_TEMPLATE['characters']['start'] = '';
-$BROWSE_TEMPLATE['characters']['body'] = 
-'
-<div class="row">
-    <div class="col-md-4">
-    			<section class="property-search-section">
-    				<div class="auto-container">
-    					<div class="property-search-tabs tabs-box">
-    						<div class="tabs-content">
-    							<!--Tab / Active Tab-->
-    							<div class="tab active-tab" id="sale">                         
-    								<div class="property-search-form">
-    									<div id="sortform">
-                                            {BROWSE_SORTBEGIN} 
-    										<div class="row">
-    											{BROWSE_CATEGORYMENU}
-    											{BROWSE_CHARACTERMENU1} {BROWSE_CHARACTERMENU2} {BROWSE_PAIRINGSMENU} {BROWSE_RATINGMENU} 
-                                                {BROWSE_CLASSMENU} 
-                                                {BROWSE_SORTMENU} 
-                                                {BROWSE_COMPLETEMENU} 
-    										</div>
-                                            {BROWSE_SORTEND}
-    									</div>									
-    								</div> 
-    							</div>
-    						</div>							
-    					</div>
-    				</div>
-    			</section>
-    </div>            
-    <div class="col-md-8">
-		{BROWSE_ALPHALINKS}
-		{BROWSE_OUTPUT}{BROWSE_OTHERRESULTS}
-		{BROWSE_SERIESBLOCK}
-        {BROWSE_PAGELINKS}
-    </div>
-</div>
-';  
-$BROWSE_TEMPLATE['characters']['end'] = ''; 
-$BROWSE_TEMPLATE['characters']['searchform'] = $BROWSE_TEMPLATE['index']['searchform'] ;  
+$BROWSE_TEMPLATE['characters'] = $BROWSE_TEMPLATE['series']; 
